94b6a40216dc61c980d6736ed2bce078b770782e
Yusuf Trihartono Tonapa <yusuftri023@gmail.com>
2023-10-02 18:24:57 +0700

    Added

---
+++ chapter 2/banking_system.js
@@ -0,0 +1,10 @@
+class BankAccount{
+    constructor(jenis_transaksi, besar_transaksi){
+        this.jenis_transaksi = jenis_transaksi
+        this.besar_transaksi = besar_transaksi
+    }
+    deposit(besar_transaksi){}
+    withdraw(besar_transaksi){}
+
+}
+function setTimeout(){}
